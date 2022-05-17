21226f78cbc052692d8cc338c01aee241404a226
GPA1992 <seuemail@exemplo.br>
2022-05-17 14:42:07 -0300

    Alteração na Resolução do Exercicio 9

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/Exercicios4-1.9.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/Exercicios4-1.9.js
@@ -1,9 +1,10 @@
 //9-Escreva um programa que defina três números em constantes e retorne true se pelo menos uma das três for ímpar. Caso contrário, ele retorna false.
 // Bonus: use somente um if.
 
-const num1 = 10;
-const num2 = 12;
-const num3 = 17;
+
+const num1 = 7;
+const num2 = 3;
+const num3 = 5;
       
     if (num1 % 2 != 0 || num2 % 2 != 0 || num3 % 2 != 0){
         console.log(true);
@@ -11,4 +12,16 @@ const num3 = 17;
     else{
         console.log(false);
     }
-    
+
+
+
+const num = [7, 4, 5]
+let result = num.some(item => item % 2 === 0);
+   if("Par"){
+       console.log(num.some(item => item % 2 === 0));
+   }
+   else{
+       console.log(num.some(item => item % 2 != 0));
+   }
+
+
