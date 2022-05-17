6d0611c0726496d438244b37407a16db6d193354
GPA1992 <seuemail@exemplo.br>
2022-05-16 21:33:33 -0300

    Iniciando Exercicios

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/Exercicios4-1.3.js
@@ -0,0 +1,6 @@
+//3-Faça um programa que retorne o maior de três números. Defina no começo do programa três constantes com os valores que serão comparados.
+let a = 5;
+let b = 6;
+let c = 7;
+
+console.log(Math.max (a, b, c));
---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/Exercicios4-1.4.js
@@ -0,0 +1,17 @@
+//4- Faça um programa que, dado um valor definido numa constante, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.
+
+let minhaIdade = "gabriel";
+
+if (minhaIdade > 18) {
+    console.log("Verdadeiro");
+}
+else if (minhaIdade < 18){
+    console.log("falso");
+} 
+else {
+    console.log(0);
+}
+
+
+
+
