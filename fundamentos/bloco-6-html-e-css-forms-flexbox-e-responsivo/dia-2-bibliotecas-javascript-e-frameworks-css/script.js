b8f9a415b83b09504f854242a8a53307a1c5f3d5
GPA <gabrielproencaaraujo@gmail.com>
2022-06-01 17:00:37 -0300

    Bootstrap instalado

---
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-2-bibliotecas-javascript-e-frameworks-css/script.js
@@ -0,0 +1,6 @@
+const sendButton = document.querySelector('#send');
+const nome = document.querySelector('#input-name');
+
+sendButton.addEventListener('click' function(event){
+event.preventDefault();
+})
